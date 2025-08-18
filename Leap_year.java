d146b59ac9096a9a9b0eb4b3fa09e715851cc3a5
chandana-harish <chandanaharish07.com>
2025-08-18 09:47:42 +0530

    variations to lap yaer program

--- Leap_year.java
+++ Leap_year.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class Leapyear {
+public class Leap_year {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter the start year");
